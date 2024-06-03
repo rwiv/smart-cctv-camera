bcf6c61a6bdf91e188debfb06b3dfbb922f84bb3
rwiv <zc798546@gmail.com>
2024-06-03 22:32:42 +0900

    feat: add mosquitto docker files

---
+++ scripts/mosquitto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ..
+
+file=./docker/docker-compose-mosquitto.yml
+
+if [ "$1" = "up" ]; then
+  sudo docker compose -f $file up -d
+elif [ "$1" = "stop" ]; then
+  sudo docker compose -f $file stop
+elif [ "$1" = "rm" ]; then
+  sudo docker compose -f $file stop
+  echo y | sudo docker compose -f $file rm
+fi
