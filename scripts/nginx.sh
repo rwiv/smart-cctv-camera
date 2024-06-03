682cc00f580af6f03feb0f94f1c50b26851b35d8
rwiv <zc798546@gmail.com>
2024-06-03 20:41:21 +0900

    feat: modify docker scripts

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,10 +2,14 @@
 
 cd ..
 
+file=./docker/docker-compose-nginx.yml
+
 if [ "$1" = "up" ]; then
   cmd="$1 -d"
-else
-  cmd="$1"
+  sudo docker compose -f $file up -d
+else if [ "$1" = "stop" ]
+  sudo docker compose -f $file stop
+else if [ "$1" = "rm" ]
+  sudo docker compose -f $file stop
+  echo y | sudo docker compose -f $file stop
 fi
-
-sudo docker compose -f ./docker/docker-compose-nginx.yml $cmd
