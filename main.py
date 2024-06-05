f186d7b4b48f47baea85951e53a33b54171139f7
rwiv <zc798546@gmail.com>
2024-06-05 11:28:00 +0900

    feat: add cors configs

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from cam_server import router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 origin 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 app.include_router(router.router)
 
